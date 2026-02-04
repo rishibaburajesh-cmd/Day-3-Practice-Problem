735702ad2ae2262f7b8a72df7b64a9b19fac85b0
Rishi Babu <rishibabu.rajesh@bridgelabz.com>
2026-02-04 16:11:15 +0530

    [Rishi Babu] added UC3: Part time employee included.

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,12 +4,25 @@ public class EmpWageComputation {
     public static void main(String[] args) {
         int wagePerHour = 20;
         int fullDayHour = 8;
+        int partTimeHour = 8;
         
         Random rn = new Random();
         System.out.println("Welcome to Employee Wage Computation");
-        boolean isPresent = rn.nextBoolean();
+        int employeeCheck = rn.nextInt(3);
 
-        int dailyWage = (isPresent) ? wagePerHour * fullDayHour : 0;
+        int dailyWage = 0;
+        int employeeHours = 0;
+        if(employeeCheck == 1) {
+            System.out.println("Full Time Employee");
+            employeeHours = fullDayHour;
+        } else if (employeeCheck == 2) {
+            System.out.println("Part Time Employee");
+            employeeHours = partTimeHour;
+        } else {
+            System.out.println("Employee is Apsent");
+        }
+
+        dailyWage = employeeHours * wagePerHour;
 
         System.out.println("Daily Employee Wage: " + dailyWage);
     }    
