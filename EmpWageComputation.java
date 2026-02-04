20f142c20b839700025be480cd90b1c92727dbaf
Rishi Babu <rishibabu.rajesh@bridgelabz.com>
2026-02-04 15:01:54 +0530

    [Rishi Babu] added UC1 features

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,3 +1,10 @@
+import java.util.Random;
 public class EmpWageComputation {
-    
+    public static void main(String[] args) {
+        Random rn = new Random();
+        System.out.println("Welcome to Employee");
+        System.out.println("Wage Computation");
+        boolean employeeStatus = rn.nextBoolean();
+        System.out.println((employeeStatus) ? "Present" : "Apsent");
+    }    
 }
